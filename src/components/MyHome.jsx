6dda18ee3bfe39833d5b2ce71524ca3def5d64d1
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-12-04 17:00:28 +0100

    main page ok

--- src/components/MyHome.jsx
+++ src/components/MyHome.jsx
@@ -1,5 +1,23 @@
+import { Container } from "react-bootstrap";
+import HeroHome from "./homepageComponents/HeroHome";
+import MainPageCards from "./homepageComponents/MainPageCards";
+
 const MyHome = () => {
-  return <></>;
+  return (
+    <>
+      <Container
+        fluid
+        style={{
+          backgroundImage:
+            "url(https://as2.ftcdn.net/v2/jpg/05/91/90/95/1000_F_591909533_UfNjf5M9QS1DgegeIgN60pTTIQyUUYqG.jpg)",
+          objectFit: "cover",
+        }}
+      >
+        <HeroHome></HeroHome>
+        <MainPageCards></MainPageCards>
+      </Container>
+    </>
+  );
 };
 
 export default MyHome;
