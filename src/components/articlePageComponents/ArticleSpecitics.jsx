d8db3f287c1d1e2d7307efcec1b8ad680f6be07e
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-12-10 18:56:07 +0100

    modale per immagine articolo

--- src/components/articlePageComponents/ArticleSpecitics.jsx
+++ src/components/articlePageComponents/ArticleSpecitics.jsx
@@ -1,41 +1,151 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Alert, Button, Col, Container, Form, Modal, Row } from "react-bootstrap";
 
 const ArticleSpecitics = () => {
+  //compilazione campi
+  const [error, setError] = useState(false);
+  const [errorText, setErrorText] = useState("");
+  const [image, setImage] = useState(null);
+  const handleSubmit = () => {
+    if (image !== null) {
+      //implementare logica da inviare al backend
+      console.log(image);
+      setImage(null);
+    } else {
+      setErrorText("per favore inserisci un'immagine ");
+      setError(true);
+    }
+  };
+  const onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setImage(URL.createObjectURL(event.target.files[0]));
+    }
+  };
+
+  //modal
+  const [show, setShow] = useState(false);
+  const [fullscreen, setFullscreen] = useState(true);
+  function handleShow(breakpoint) {
+    setFullscreen(breakpoint);
+    setShow(true);
+  }
+
+  //-------------------------
   return (
-    <Container>
-      <Row>
-        <Col xs={5} className="text-light p-0">
-          <Container className="p-0 px-3" style={{ borderBottom: "solid 3px #89C0F2" }}>
-            <p className="text-center mb-0 fw-bold fs-4">TITOLO:</p>
-            <p className="fs-4 m-0">Doom</p>
-          </Container>
-          <Container className="p-0 px-3" style={{ borderBottom: "solid 3px #89C0F2" }}>
-            <p className="text-center mb-0 fw-bold fs-4">SVILUPPATORE:</p>
-            <p className="fs-4 m-0">ID SOFTWARE</p>
-          </Container>
-          <Container className="p-0 px-3" style={{ borderBottom: "solid 3px #89C0F2" }}>
-            <p className="text-center mb-0 fw-bold fs-4">PUBBLICAZIONE:</p>
-            <p className="fs-4 m-0">ID SOFTWARE</p>
-          </Container>
-          <Container className="p-0 px-3" style={{ borderBottom: "solid 3px #89C0F2" }}>
-            <p className="text-center mb-0 fw-bold fs-4">TEMA:</p>
-            <p className="fs-4 m-0">FANTASCIENTIFICO</p>
-          </Container>
-          <Container className="p-0 px-3">
-            <p className="text-center mb-0 fw-bold fs-4">GENERE:</p>
-            <p className="fs-4 m-0">SPARATUTTO , RETRO , OLD SCHOOL</p>
-          </Container>
-        </Col>
-        <Col xs={7} className="p-0" style={{ minHeight: "400px" }}>
-          <img
-            src="https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/H2x1_NSwitchDS_DOOM1993_image1600w.jpg"
-            alt="cover"
-            height={"100%"}
-            width={"100%"}
-          />
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container>
+        <Row>
+          <Col xs={5} className="text-light p-0">
+            <Container className="p-0 px-3" style={{ borderBottom: "solid 3px #89C0F2" }}>
+              <p className="text-center mb-0 fw-bold fs-4">TITOLO:</p>
+              <p className="fs-4 m-0">Doom</p>
+            </Container>
+            <Container className="p-0 px-3" style={{ borderBottom: "solid 3px #89C0F2" }}>
+              <p className="text-center mb-0 fw-bold fs-4">SVILUPPATORE:</p>
+              <p className="fs-4 m-0">ID SOFTWARE</p>
+            </Container>
+            <Container className="p-0 px-3" style={{ borderBottom: "solid 3px #89C0F2" }}>
+              <p className="text-center mb-0 fw-bold fs-4">PUBBLICAZIONE:</p>
+              <p className="fs-4 m-0">ID SOFTWARE</p>
+            </Container>
+            <Container className="p-0 px-3" style={{ borderBottom: "solid 3px #89C0F2" }}>
+              <p className="text-center mb-0 fw-bold fs-4">TEMA:</p>
+              <p className="fs-4 m-0">FANTASCIENTIFICO</p>
+            </Container>
+            <Container className="p-0 px-3">
+              <p className="text-center mb-0 fw-bold fs-4">GENERE:</p>
+              <p className="fs-4 m-0">SPARATUTTO , RETRO , OLD SCHOOL</p>
+            </Container>
+          </Col>
+          <Col xs={7} className="p-0" style={{ minHeight: "400px", position: "relative" }}>
+            <img
+              src="https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/H2x1_NSwitchDS_DOOM1993_image1600w.jpg"
+              alt="cover"
+              height={"100%"}
+              width={"100%"}
+            />
+            <Button
+              className="p-1 py-0"
+              style={{
+                position: "absolute",
+                right: "10px",
+                top: "5px",
+                backgroundColor: "rgb(36 112 222 / 62%)",
+                width: "fit-content",
+              }}
+              onClick={() => {
+                handleShow("lg-down");
+              }}
+            >
+              <i className="bi bi-pen text-light"></i>
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+
+      <Modal
+        show={show}
+        fullscreen={fullscreen}
+        onHide={() => {
+          setShow(false);
+          setImage(null);
+        }}
+      >
+        {error && (
+          <Alert
+            variant="danger"
+            onClose={() => {
+              setError(false);
+            }}
+            dismissible
+          >
+            <Alert.Heading>error!</Alert.Heading>
+            <p>{errorText}</p>
+          </Alert>
+        )}
+        <Modal.Header closeButton className="bg-white" style={{ filter: "invert(1) grayscale(100%) brightness(200%)" }}>
+          <Modal.Title>immagine</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="bg-black text-light">
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <input
+              type="file"
+              autoFocus
+              accept="image/*"
+              onChange={(e) => onImageChange(e)}
+              style={{
+                filter: "invert(1) grayscale(100%) brightness(200%)",
+              }}
+              className="text-dark"
+            />
+            <Container className="notFilter my-2">
+              {image && (
+                <img
+                  alt="preview"
+                  src={image}
+                  className="img-fluid my-2"
+                  style={{
+                    border: "solid 3px  white",
+                  }}
+                />
+              )}
+            </Container>
+          </Form.Group>
+          <Button
+            className="fw-bold btn-light"
+            style={{
+              fontSize: "40px",
+              width: "100%",
+              border: "solid 3px  #89C0F2",
+              filter: "invert(1) grayscale(100%) brightness(200%)",
+            }}
+            onClick={() => handleSubmit()}
+          >
+            invia
+          </Button>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 };
 
